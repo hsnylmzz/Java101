edbc151b934098bb58206f17948d48e5fc1dc848
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-01 16:02:44 +0300

    Add files via upload

---
+++ KdvTutarHesaplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class KdvTutarHesaplama {
+
+	public static void main(String[] args) {
+		double kdvliTutar,kdvTutar,tutar,kdvOran = 0.18;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Ücret Tutarýný Giriniz : ");
+		tutar = input.nextDouble();
+		
+		kdvTutar = tutar*kdvOran ;
+        kdvliTutar = tutar+kdvTutar;
+        kdvOran = (tutar * 0.18);
+
+        System.out.println("Kdv'siz Tutar :"+tutar);
+        System.out.println("Kdvli Tutar   :" +kdvliTutar);
+        System.out.println("Kdv Tutarý    :"+kdvTutar);
+        System.out.println("Kdv Oraný     :"+kdvOran);
+
+	}
+
+}

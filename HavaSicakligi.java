63dd2ea4f5258624b77eb06d781afb746a8b72cb
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-02 20:27:48 +0300

    Update HavaSicakligi.java

--- HavaSicakligi.java
+++ HavaSicakligi.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class HavaSicakligi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner scan=new Scanner(System.in);
+        int sicaklik;
+        System.out.println("Sıcaklık değerini giriniz:");
+        sicaklik=scan.nextInt();
 
-	}
+        if (sicaklik<5)
+        {
+            System.out.println("Kayak yapabilirsiniz");
+        }
+        else if (sicaklik>=5 && sicaklik<=15)
+        {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        }
+        else if (sicaklik>=15 && sicaklik<=25)
+        {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        }
+        else 
+        {
+            System.out.println("Yüzmeye gidebilirsiniz:");
+        }
 
+   }
 }
+

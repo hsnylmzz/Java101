fd7d4b7193f314cff0150480680c84bb0fe5a78a
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-03 18:28:45 +0300

    Update CiftDortKatlari.java

--- CiftDortKatlari.java
+++ CiftDortKatlari.java
@@ -5,10 +5,10 @@ public class CiftDortKatlari {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n;
-        int toplam= 0;
+                int toplam= 0;
 
         do {
-            System.out.print("Sayý Giriniz : ");
+            System.out.print("SayÃ½ Giriniz : ");
             n = input.nextInt();
             if (n % 2 == 0 && (n % 4 == 0))  {
                 toplam += n;

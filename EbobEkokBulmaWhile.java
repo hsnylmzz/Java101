23b5a674c2858806374656874eb66a1b4b817384
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-04 19:40:10 +0300

    Add files via upload

---
+++ EbobEkokBulmaWhile.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EbobEkokBulmaWhile {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.print("n1 sayýsýný giriniz :");
+        int n1 = input.nextInt();
+        System.out.print("n2 sayýsýný giriniz : ");
+        int n2 = input.nextInt();
+
+        int k = n1;
+        int ebob = 1;
+        int ekok = 1;
+
+        while (k > 1) {
+            if (n1 % k == 0 && n2 % k == 0) {
+                ebob = k;
+                ekok = (n1 * n2) / ebob;
+                break;
+            }
+            k--;
+
+
+        }
+        System.out.println(" Ebob : " + ebob + " Ekok : " + ekok);
+
+
+    }
+
+
+}
+
+

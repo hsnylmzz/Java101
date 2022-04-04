ab1ea808f70e18d8d15c24b1bc85feff749edc3d
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-04 18:24:00 +0300

    Update HarmonikSayiBulma.java

--- HarmonikSayiBulma.java
+++ HarmonikSayiBulma.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class HarmonikSayiBulma {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-        System.out.print("Harmonik Serisi Bulunacak Sayý Giriniz : ");
+	Scanner input = new Scanner(System.in);
+        System.out.print("Harmonik Serisi Bulunacak SayÃ½ Giriniz : ");
         int n = input.nextInt();
         double harmonik = 0;
         for (int i =1; i <= n; i++)
         {
             harmonik += (1.0/i);
         }
-        System.out.println("Sayýnýn Harmonik Deðeri : " + harmonik);
+        System.out.println("SayÃ½nÃ½n Harmonik DeÃ°eri : " + harmonik);
 
 	}
 

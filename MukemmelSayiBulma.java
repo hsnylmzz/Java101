da29b992b6c16a0d050aa48993c6957db7a49792
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-04 19:48:50 +0300

    Add files via upload

---
+++ MukemmelSayiBulma.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MukemmelSayiBulma {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+        System.out.print("Sayý giriniz: ");
+        int sayi=input.nextInt();
+        int toplam=0;
+        for (int i=1; i<sayi; i++){
+            if (sayi%i==0){
+                toplam+=i;
+            }
+        }
+        if (toplam==sayi)
+            System.out.println("Girdiðiniz sayý mükemmel sayýdýr.");
+        else
+            System.out.println("Girdiðiniz sayý mükemmel sayý deðildir.");
+
+	}
+
+}

929c20b3ca14b8b3d1d5be5b30a117fec22e7462
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-05 17:25:23 +0300

    Add files via upload

---
+++ AsalBulma.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class AsalBulma {
+	
+	static int asal(int i,int sayi)
+	{
+		if(sayi == i)
+		{
+			return 0;
+		}
+		else if(sayi % i == 0)
+		{
+			return 1;
+		}
+		else {
+			return asal(i+1,sayi);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Sayiyi giriniz: ");
+        int kontrol = input.nextInt();
+
+        if (asal(2, kontrol)==0){
+            System.out.print(kontrol+" Asal sayidir");
+        }else {
+            System.out.print(kontrol+" Asal degildir");
+        }
+
+	}
+
+}

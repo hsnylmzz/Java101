c8c6b702a48a6908a430788ddbbaf797192734b4
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2022-04-06 17:34:45 +0300

    Update Main.java

--- OgrenciBilgiSistemi/src/Main.java
+++ OgrenciBilgiSistemi/src/Main.java
@@ -2,26 +2,26 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Teacher t1 = new Teacher("Mehmet Yılmaz","213","TRH");
+		Teacher t1 = new Teacher("Mehmet YÃ½lmaz","213","TRH");
 		Teacher t2 = new Teacher("Isaac Newton","342","FZK");
-		Teacher t3 = new Teacher("Üç Kağıtçı Sinan","1111","BİO");
+		Teacher t3 = new Teacher("ÃœÃ§ KaÃ°Ã½tÃ§Ã½ Sinan","1111","BÃO");
 		Course tarih = new Course("Tarih","101","TRH");
 		tarih.addTeacher(t1);
 		Course fizik = new Course("Fizik","102","FZK");
 		fizik.addTeacher(t2);
-		Course biyo = new Course("Biyoloji","106","BİO");
+		Course biyo = new Course("Biyoloji","106","BÃO");
 		biyo.addTeacher(t3);
-		Student s1 = new Student("Hasan Yılmaz","12","4",tarih,fizik,biyo);
+		Student s1 = new Student("Hasan YÃ½lmaz","12","4",tarih,fizik,biyo);
 		s1.addBulkExamNote(100, 80, 70);
 		s1.opinionNote(95,85,75);
 		s1.printNote();
 		s1.isPass();
 		
-		Student s2 = new Student("Güdük Necmi","4","214142", tarih, fizik, biyo);
+		Student s2 = new Student("GÃ¼dÃ¼k Necmi","4","214142", tarih, fizik, biyo);
 		s2.addBulkExamNote(80, 20, 70);
 		s2.opinionNote(100,80,70);
 		s2.printNote();
-        s2.isPass();
+                s2.isPass();
 	}
 
 }
